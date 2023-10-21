30db1c3e37be74b8b4ec6c93a1cb9cfff1572ff1
layan <layanb214@gmail.com>
2023-10-21 15:05:10 -0700

    commit

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 let add = document.getElementById("add");
 
 add.addEventListener("click", function() {
-    add.textContent = "haha"
+    add.textContent = "add"
 })
+
+
+let background = document.getElementById("change background");
+
+function newcolour(){
+    var symbols,colour;
+    symbols = "0123456789ABCDEF"
+    colour = "#";
+    for(var i = 0; i<6; i++){
+        colour = colour + symbols[Math.floor(Math.random() * 16)];
+    
+    }
+    document.content.background = colour;
+}

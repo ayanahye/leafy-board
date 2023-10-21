8e2f08beea793d8f0d5b93d8a842f9b844487d0e
ayanahye <ayanahye2@gmail.com>
2023-10-21 15:27:32 -0700

    hi

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 let add = document.getElementById("add");
+const textonleaf = document.querySelector('.textonleaf');
 
-add.addEventListener("click", function() {
-    add.textContent = "haha"
-})
+textonleaf.setAttribute('draggable', 'true');
+
+textonleaf.addEventListener('dragstart', function (ev) {
+    ev.dataTransfer.setData('text', ev.target.id);
+});
+
+add.addEventListener('dragover', function (ev) {
+    ev.preventDefault();
+});
+
+add.addEventListener('drop', function (ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData('text');
+    ev.target.appendChild(document.getElementById(data));
+});

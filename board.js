9116d8491fed4aef11bfe19a60f3d8e0a7e054f3
Poorvi <poorvi.b26@gmail.com>
2023-10-21 15:34:07 -0700

    uhh

--- board.js
+++ board.js
@@ -1,22 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     // Get the edit button element
-//     var editButton = document.getElementById("edit");
-
-//     // Add a click event listener to the edit button
-//     editButton.addEventListener("click", function() {
-//         // Prompt the user to enter a color
-//         var chosenColor = prompt("Choose a color (e.g., red, #00ff00):");
-
-//         // Check if the user entered a color
-//         if (chosenColor !== null && chosenColor !== "") {
-//             // Set the background color of the frame
-//             document.querySelector(".frame").style.backgroundColor = chosenColor;
-//         }
-//     });
-// });
-
-// app.js
-
 document.addEventListener("DOMContentLoaded", function() {
     // Get the edit button element
     var editButton = document.getElementById("edit");

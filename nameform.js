071838977a0798af83b046d70f13a68ca01c4e66
gurveeng <gurveenngill@gmail.com>
2023-10-21 14:42:33 -0700

    name form

---
+++ nameform.js
@@ -0,0 +1,9 @@
+
+const nameForm = document.getElementById("nameForm");
+const output = document.getElementById("output");
+
+nameForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    output.textContent = `${name}'s Leafy Board:`;
+});

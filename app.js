5e6637b38f8de727c6b3c7e70b6869c27f3bf888
ayanahye <ayanahye2@gmail.com>
2023-10-21 15:50:06 -0700

    hi

--- app.js
+++ app.js
@@ -1,32 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const addButton = document.getElementById("add");
+    const leafText = document.getElementById("leafText");
+    const leafImage = document.querySelector(".image-leaf");
 
-add.addEventListener("click", function() {
-    add.textContent = "add"
-})
+    addButton.addEventListener("click", function () {
+        // Get the dimensions of the .textwleaf container
+        const containerWidth = document.querySelector(".textwleaf").offsetWidth;
+        const containerHeight = document.querySelector(".textwleaf").offsetHeight;
 
+        // Generate random left and top values within the container
+        const randomLeft = Math.floor(Math.random() * (containerWidth - 200)); // 200 is the width of the elements
+        const randomTop = Math.floor(Math.random() * (containerHeight - 200)); // 200 is the height of the elements
 
-let background = document.getElementById("change background");
+        // Update the left and top properties of the elements
+        leafText.style.left = randomLeft + "px";
+        leafText.style.top = randomTop + "px";
+        leafText.style.display = "block"; // Show the leaf text
 
-function newcolour(){
-    var symbols,colour;
-    symbols = "0123456789ABCDEF"
-    colour = "#";
-    for(var i = 0; i<6; i++){
-        colour = colour + symbols[Math.floor(Math.random() * 16)];
-    
-    }
-    document.content.background = colour;
-}
-
-var changeColorButton = document.getElementById("changeColorButton");
-changeColorButton.addEventListener("click", newcolour);
-
-
-const nameForm = document.getElementById("nameForm");
-const output = document.getElementById("output");
-
-nameForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-    const name = document.getElementById("name").value;
-    output.textContent = `${name}'s Leafy Board:`;
+        leafImage.style.left = randomLeft + "px";
+        leafImage.style.top = randomTop + "px";
+    });
 });
-

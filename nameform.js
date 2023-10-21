648cd0ddf8c13ee52da3ee48d14517adc6ffb2a8
gurveeng <gurveenngill@gmail.com>
2023-10-21 15:06:16 -0700

    fixed fs

--- nameform.js
+++ nameform.js
@@ -6,4 +6,5 @@ nameForm.addEventListener("submit", function (e) {
     e.preventDefault();
     const name = document.getElementById("name").value;
     output.textContent = `${name}'s Leafy Board:`;
+    document.getElementById("link").click();
 });

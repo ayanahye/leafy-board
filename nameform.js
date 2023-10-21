ae5c57aafa4cdbcb8b786b257189616a9bdee19e
gurveeng <gurveenngill@gmail.com>
2023-10-21 15:17:50 -0700

    ijrnei

--- nameform.js
+++ nameform.js
@@ -17,3 +17,11 @@ if (nameParam) {
     const outputElement = document.getElementById("output");
     outputElement.textContent = `${nameParam}'s Leafy Board:`;
 }
+
+document.getElementById("nameForm").addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent the default form submission behavior
+
+    const name = document.getElementById("name").value;
+    // Redirect to home.html with the name parameter in the URL
+    window.location.href = `home.html?name=${name}`;
+});
